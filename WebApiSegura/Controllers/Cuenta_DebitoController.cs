cb701480cd4ef4ac33708b0afb0409ee4c9a9e56
André L <52118901+Anderlyn@users.noreply.github.com>
2021-07-15 21:47:06 -0600

    Controllers de Andy

---
+++ WebApiSegura/Controllers/Cuenta_DebitoController.cs
@@ -0,0 +1,226 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebApiSegura.Models;
+
+namespace WebApiSegura.Controllers
+{
+    [AllowAnonymous]
+    [RoutePrefix("api/Cuenta_Debito")]
+    public class Cuenta_DebitoController : ApiController
+    {
+        [HttpGet]
+        public IHttpActionResult GetId(int id)
+        {
+            Cuenta_Debito cuenta_debito = new Cuenta_Debito();
+            try
+            {
+                using (SqlConnection sqlConnection = new
+                    SqlConnection(ConfigurationManager.ConnectionStrings["INTERNET_BANKING"].ConnectionString))
+                {
+                    SqlCommand sqlCommand = new SqlCommand(@"SELECT Codigo, CodigoUsuario, CodigoMoneda, CodigoSucursal,CodigoTarjeta,
+                                                             Descripcion, IBAN, Saldo, Estado
+                                                             FROM   Cuenta_Debito
+                                                             WHERE Codigo = @Codigo", sqlConnection);
+
+                    sqlCommand.Parameters.AddWithValue("@Codigo", id);
+
+                    sqlConnection.Open();
+
+                    SqlDataReader sqlDataReader = sqlCommand.ExecuteReader();
+
+                    while (sqlDataReader.Read())
+                    {
+                        cuenta_debito.Codigo = sqlDataReader.GetInt32(0);
+                        cuenta_debito.CodigoUsuario = sqlDataReader.GetInt32(1);
+                        cuenta_debito.CodigoMoneda = sqlDataReader.GetInt32(2);
+                        cuenta_debito.CodigoSucursal = sqlDataReader.GetInt32(3);
+                        cuenta_debito.CodigoTarjeta = sqlDataReader.GetInt32(4);
+                        cuenta_debito.Descripcion = sqlDataReader.GetString(5);
+                        cuenta_debito.IBAN = sqlDataReader.GetString(6);
+                        cuenta_debito.Saldo = sqlDataReader.GetDecimal(7);
+                        cuenta_debito.Estado = sqlDataReader.GetString(8);
+                    }
+
+                    sqlConnection.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                return InternalServerError(ex);
+            }
+
+            return Ok(cuenta_debito);
+        }
+
+        [HttpGet]
+        public IHttpActionResult GetAll()
+        {
+            List<Cuenta_Debito> cuentas_debito = new List<Cuenta_Debito>();
+            try
+            {
+                using (SqlConnection sqlConnection = new
+                    SqlConnection(ConfigurationManager.ConnectionStrings["INTERNET_BANKING"].ConnectionString))
+                {
+                    SqlCommand sqlCommand = new SqlCommand(@"SELECT Codigo, CodigoUsuario, CodigoMoneda, CodigoSucursal,CodigoTarjeta,
+                                                             Descripcion, IBAN, Saldo, Estado
+                                                             FROM   Cuenta_Debito", sqlConnection);
+                    sqlConnection.Open();
+
+                    SqlDataReader sqlDataReader = sqlCommand.ExecuteReader();
+
+                    while (sqlDataReader.Read())
+                    {
+                        Cuenta_Debito cuenta_debito = new Cuenta_Debito();
+
+                        cuenta_debito.Codigo = sqlDataReader.GetInt32(0);
+                        cuenta_debito.CodigoUsuario = sqlDataReader.GetInt32(1);
+                        cuenta_debito.CodigoMoneda = sqlDataReader.GetInt32(2);
+                        cuenta_debito.CodigoSucursal = sqlDataReader.GetInt32(3);
+                        cuenta_debito.CodigoTarjeta = sqlDataReader.GetInt32(4);
+                        cuenta_debito.Descripcion = sqlDataReader.GetString(5);
+                        cuenta_debito.IBAN = sqlDataReader.GetString(6);
+                        cuenta_debito.Saldo = sqlDataReader.GetDecimal(7);
+                        cuenta_debito.Estado = sqlDataReader.GetString(8);
+
+                        cuentas_debito.Add(cuenta_debito);
+                    }
+                    sqlConnection.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                return InternalServerError(ex);
+            }
+            return Ok(cuentas_debito);
+        }
+
+
+        [HttpPost]
+        public IHttpActionResult Ingresar(Cuenta_Debito cuenta_debito)
+        {
+            if (cuenta_debito == null)
+                return BadRequest();
+
+            try
+            {
+                using (SqlConnection sqlConnection =
+                    new SqlConnection(ConfigurationManager.ConnectionStrings["INTERNET_BANKING"].ConnectionString))
+                {
+                    SqlCommand sqlCommand =
+                        new SqlCommand(@" INSERT INTO Cuenta_Debito (CodigoUsuario, CodigoMoneda, CodigoSucursal,CodigoTarjeta, Descripcion, 
+                                                                IBAN, Saldo, Estado) 
+                                         VALUES (@CodigoUsuario, @CodigoMoneda, @CodigoSucursal,@CodigoTarjeta, @Descripcion, @IBAN, @Saldo, @Estado)",
+                                         sqlConnection);
+
+                    sqlCommand.Parameters.AddWithValue("@CodigoUsuario", cuenta_debito.CodigoUsuario);
+                    sqlCommand.Parameters.AddWithValue("@CodigoMoneda", cuenta_debito.CodigoMoneda);
+                    sqlCommand.Parameters.AddWithValue("@CodigoSucursal", cuenta_debito.CodigoSucursal);
+                    sqlCommand.Parameters.AddWithValue("@CodigoTarjeta", cuenta_debito.CodigoTarjeta);
+                    sqlCommand.Parameters.AddWithValue("@Descripcion", cuenta_debito.Descripcion);
+                    sqlCommand.Parameters.AddWithValue("@IBAN", cuenta_debito.IBAN);
+                    sqlCommand.Parameters.AddWithValue("@Saldo", cuenta_debito.Saldo);
+                    sqlCommand.Parameters.AddWithValue("@Estado", cuenta_debito.Estado);
+
+                    sqlConnection.Open();
+
+                    int filasAfectadas = sqlCommand.ExecuteNonQuery();
+
+                    sqlConnection.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                return InternalServerError(ex);
+            }
+
+            return Ok(cuenta_debito);
+        }
+
+        [HttpPut]
+        public IHttpActionResult Actualizar(Cuenta_Debito cuenta_debito)
+        {
+            if (cuenta_debito == null)
+                return BadRequest();
+
+            try
+            {
+                using (SqlConnection sqlConnection =
+                    new SqlConnection(ConfigurationManager.ConnectionStrings["INTERNET_BANKING"].ConnectionString))
+                {
+                    SqlCommand sqlCommand =
+                        new SqlCommand(@" UPDATE Cuenta_Debito
+                                                        SET CodigoUsuario = @CodigoUsuario, 
+                                                            CodigoMoneda = @CodigoMoneda,
+CodigoSucursal = @CodigoSucursal,
+CodigoTarjeta = @CodigoTarjeta ,
+                                                            Descripcion = @Descripcion, 
+                                                            IBAN = @IBAN, 
+                                                            Saldo = @Saldo, 
+                                                            Estado = @Estado 
+                                          WHERE Codigo = @Codigo",
+                                         sqlConnection);
+
+                    sqlCommand.Parameters.AddWithValue("@Codigo", cuenta_debito.Codigo);
+                    sqlCommand.Parameters.AddWithValue("@CodigoUsuario", cuenta_debito.CodigoUsuario);
+                    sqlCommand.Parameters.AddWithValue("@CodigoMoneda", cuenta_debito.CodigoMoneda);
+                    sqlCommand.Parameters.AddWithValue("@CodigoSucursal", cuenta_debito.CodigoSucursal);
+                    sqlCommand.Parameters.AddWithValue("@CodigoTarjeta", cuenta_debito.CodigoTarjeta);
+                    sqlCommand.Parameters.AddWithValue("@Descripcion", cuenta_debito.Descripcion);
+                    sqlCommand.Parameters.AddWithValue("@IBAN", cuenta_debito.IBAN);
+                    sqlCommand.Parameters.AddWithValue("@Saldo", cuenta_debito.Saldo);
+                    sqlCommand.Parameters.AddWithValue("@Estado", cuenta_debito.Estado);
+
+                    sqlConnection.Open();
+
+                    int filasAfectadas = sqlCommand.ExecuteNonQuery();
+
+                    sqlConnection.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                return InternalServerError(ex);
+            }
+
+            return Ok(cuenta_debito);
+        }
+
+        [HttpDelete]
+        public IHttpActionResult Eliminar(int id)
+        {
+            if (id < 1)
+                return BadRequest();
+
+            try
+            {
+                using (SqlConnection sqlConnection =
+                    new SqlConnection(ConfigurationManager.ConnectionStrings["INTERNET_BANKING"].ConnectionString))
+                {
+                    SqlCommand sqlCommand =
+                        new SqlCommand(@" DELETE Cuenta_Debito WHERE Codigo = @Codigo",
+                                         sqlConnection);
+
+                    sqlCommand.Parameters.AddWithValue("@Codigo", id);
+
+                    sqlConnection.Open();
+
+                    int filasAfectadas = sqlCommand.ExecuteNonQuery();
+
+                    sqlConnection.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                return InternalServerError(ex);
+            }
+
+            return Ok(id);
+        }
+    }
+}
---
+++ WebApiSegura/Controllers/EmisorController.cs
@@ -0,0 +1,204 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebApiSegura.Models;
+
+
+namespace WebApiSegura.Controllers
+{
+    [AllowAnonymous]
+    [RoutePrefix("api/Emisor")]
+    public class EmisorController : ApiController
+    {
+
+        [HttpGet]
+        public IHttpActionResult GetId(int id)
+        {
+            Emisor emisor = new Emisor();
+            try
+            {
+                using (SqlConnection sqlConnection = new
+                    SqlConnection(ConfigurationManager.ConnectionStrings["INTERNET_BANKING"].ConnectionString))
+                {
+                    SqlCommand sqlCommand = new SqlCommand(@"SELECT Codigo, Descripcion, Prefijo,NumeroDigitos
+                                                             FROM   Emisor
+                                                             WHERE Codigo = @Codigo", sqlConnection);
+
+                    sqlCommand.Parameters.AddWithValue("@Codigo", id);
+
+                    sqlConnection.Open();
+
+                    SqlDataReader sqlDataReader = sqlCommand.ExecuteReader();
+
+                    while (sqlDataReader.Read())
+                    {
+                        emisor.Codigo = sqlDataReader.GetInt32(0);
+                        emisor.Descripcion = sqlDataReader.GetString(1);
+                        emisor.Prefijo = sqlDataReader.GetString(2);
+                        emisor.NumeroDigitos = sqlDataReader.GetInt32(3);
+                    }
+
+                    sqlConnection.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                return InternalServerError(ex);
+            }
+
+            return Ok(emisor);
+        }
+
+        [HttpGet]
+        public IHttpActionResult GetAll()
+        {
+            List<Emisor> emisores = new List<Emisor>();
+            try
+            {
+                using (SqlConnection sqlConnection = new
+                    SqlConnection(ConfigurationManager.ConnectionStrings["INTERNET_BANKING"].ConnectionString))
+                {
+                    SqlCommand sqlCommand = new SqlCommand(@"SELECT Codigo, Descripcion, Prefijo,NumeroDigitos
+                                                             FROM   Emisor", sqlConnection);
+                    sqlConnection.Open();
+
+                    SqlDataReader sqlDataReader = sqlCommand.ExecuteReader();
+
+                    while (sqlDataReader.Read())
+                    {
+                        Emisor emisor = new Emisor();
+                        emisor.Codigo = sqlDataReader.GetInt32(0);
+                        emisor.Descripcion = sqlDataReader.GetString(1);
+                        emisor.Prefijo = sqlDataReader.GetString(2);
+                        emisor.NumeroDigitos = sqlDataReader.GetInt32(3);
+
+                        emisores.Add(emisor);
+                    }
+                    sqlConnection.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                return InternalServerError(ex);
+            }
+            return Ok(emisores);
+        }
+
+
+        [HttpPost]
+        public IHttpActionResult Ingresar(Emisor emisor)
+        {
+            if (emisor == null)
+                return BadRequest();
+
+            try
+            {
+                using (SqlConnection sqlConnection =
+                    new SqlConnection(ConfigurationManager.ConnectionStrings["INTERNET_BANKING"].ConnectionString))
+                {
+                    SqlCommand sqlCommand =
+                        new SqlCommand(@" INSERT INTO Emisor (Descripcion, Prefijo,NumeroDigitos) 
+                                         VALUES (@Descripcion, @Prefijo, @NumeroDigitos)",
+                                         sqlConnection);
+
+
+                    sqlCommand.Parameters.AddWithValue("@Descripcion", emisor.Descripcion);
+                    sqlCommand.Parameters.AddWithValue("@Prefijo", emisor.Prefijo);
+                    sqlCommand.Parameters.AddWithValue("@NumeroDigitos", emisor.NumeroDigitos);
+
+                    sqlConnection.Open();
+
+                    int filasAfectadas = sqlCommand.ExecuteNonQuery();
+
+                    sqlConnection.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                return InternalServerError(ex);
+            }
+
+            return Ok(emisor);
+        }
+
+        [HttpPut]
+        public IHttpActionResult Actualizar(Emisor emisor)
+        {
+            if (emisor == null)
+                return BadRequest();
+
+            try
+            {
+                using (SqlConnection sqlConnection =
+                    new SqlConnection(ConfigurationManager.ConnectionStrings["INTERNET_BANKING"].ConnectionString))
+                {
+                    SqlCommand sqlCommand =
+                        new SqlCommand(@" UPDATE Emisor
+                                                        SET 
+                                                            Descripcion = @Descripcion,
+                                                            Prefijo = @Prefijo,
+                                                             NumeroDigitos = @NumeroDigitos
+                                          WHERE Codigo = @Codigo",
+                                         sqlConnection);
+
+
+                    sqlCommand.Parameters.AddWithValue("@Codigo", emisor.Codigo);
+                    sqlCommand.Parameters.AddWithValue("@Descripcion", emisor.Descripcion);
+                    sqlCommand.Parameters.AddWithValue("@Prefijo", emisor.Prefijo);
+                    sqlCommand.Parameters.AddWithValue("@NumeroDigitos", emisor.NumeroDigitos);
+
+                    sqlConnection.Open();
+
+                    int filasAfectadas = sqlCommand.ExecuteNonQuery();
+
+                    sqlConnection.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                return InternalServerError(ex);
+            }
+
+            return Ok(emisor);
+        }
+
+        [HttpDelete]
+        public IHttpActionResult Eliminar(int id)
+        {
+            if (id < 1)
+                return BadRequest();
+
+            try
+            {
+                using (SqlConnection sqlConnection =
+                    new SqlConnection(ConfigurationManager.ConnectionStrings["INTERNET_BANKING"].ConnectionString))
+                {
+                    SqlCommand sqlCommand =
+                        new SqlCommand(@" DELETE Emisor WHERE Codigo = @Codigo",
+                                         sqlConnection);
+
+                    sqlCommand.Parameters.AddWithValue("@Codigo", id);
+
+                    sqlConnection.Open();
+
+                    int filasAfectadas = sqlCommand.ExecuteNonQuery();
+
+                    sqlConnection.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                return InternalServerError(ex);
+            }
+
+            return Ok(id);
+        }
+    }
+}
+
+
